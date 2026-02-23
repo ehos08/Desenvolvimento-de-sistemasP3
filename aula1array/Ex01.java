67967e2f683eadae0c5494666b31b3b44e670489
sahmlo <smsj318@gmail.com>
2026-02-23 15:16:50 -0300

    aula array

---
+++ aula1array/Ex01.java
@@ -0,0 +1,7 @@
+public class Ex01 {
+    public static void main(String[] args) {
+        for(int i=1;i<=16;i++){
+            System.out.println("Contador: " +i);
+        }
+    }
+}
---
+++ aula1array/Ex03.java
@@ -0,0 +1,10 @@
+public class Ex03 {
+    
+    public static void main(String[] args) {
+        for(int i=100;i>=1;i--){
+            if (i%2!=0){
+             System.out.println("contador de ímpares: " +i);
+            }
+        }
+    }
+}
